2f0a7b7b539459e5292d8b824b7ca9baf9bb06c3
md-masum <masum.nextu@gmail.com>
2023-03-29 08:56:25 +0600

    function complete on the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,9 +4,14 @@ import './Cart.css';
 const Cart = ({cart}) => {
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     let tax = totalPrice*7/100;
     let grandTotal = totalPrice + totalShipping + tax;
@@ -14,7 +19,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h3>Order Summary:</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
